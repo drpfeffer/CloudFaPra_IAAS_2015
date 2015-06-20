8666535453c4757fd952579d253e25f3e639219b
Julian <MyFakeAcc.4@googlemail.com>
2015-06-20 17:11:17 +0200

    4.4: fix

--- A4/4/deploy-bluechatter.sh
+++ A4/4/deploy-bluechatter.sh
@@ -9,9 +9,18 @@ USER="fapra@felix-ebinger.de"
 SUF=$RANDOM
 
 cf login -a https://api.ng.bluemix.net -u $USER -o $ORG -s $SPACE
-cf create-service rediscloud 30mb redis-chatter
+cf create-service rediscloud 30mb redis-chatter || true
 git clone https://github.com/CodenameBlueMix/bluechatter.git "bluechatter_${SUF}"
-gsed -r -i 's|^\s+plan:\s+25mb$|    plan:  30mb|g' bluechatter_22164/manifest.yml # gnu sed needed
+
+# the manifast takes care of the service relations, but we need to fix it first
+# unfortunately, the needed flags differ between BSD and GNU sed
+# use --help as cheap detection for GNU sed (there's no such flag in BSD sed)
+if sed --help &> /dev/null; then
+    sed -r -i 's|^\s+plan:\s+25mb$|    plan:  30mb|g' "bluechatter_${SUF}"/manifest.yml
+else
+    sed -E -i '' 's|^\s+plan:\s+25mb$|    plan:  30mb|g' "bluechatter_${SUF}"/manifest.yml
+fi
+
 cd "bluechatter_${SUF}"
 cf push fapra-bluechatter
 cd ..
